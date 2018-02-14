1c2326b5b0ebdcf98b164e3324f391c40974f3fe
patrickgrey <websemantic@yahoo.co.uk>
2018-02-14 11:38:31 +0000

    Added a riddle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
-var unicorn = document.getElementById("unicornMoving");
-var blueRectangle = document.getElementById("blueRectangle");
+var unicorn = document.getElementById('unicornMoving');
+var blueRectangle = document.getElementById('blueRectangle');
 
-var how_far = 300;
+var how_far = 30;
 var time = 0.5;
 
 var play_sound = function() {
-  var sound = new Audio("sounds/unicorn.mp3");
+  var sound = new Audio('sounds/unicorn.mp3');
   sound.play();
 };
 
-$("#unicornMoving").click(function(event) {
+$('#unicornMoving').click(function(event) {
   TweenMax.to(unicorn, time, {
-    x: "+=" + how_far + "px",
+    x: '+=' + how_far + 'px',
     onStart: play_sound,
     onComplete: I_am_complete
   });
@@ -24,17 +24,17 @@ $("#unicornMoving").click(function(event) {
 var I_am_complete = function() {
   var position = unicorn.getBoundingClientRect();
   // console.log(position.left);
-  console.log("I am unicorn and I am in position: ", position.right);
+  console.log('I am unicorn and I am in position: ', position.right);
 
   var width_of_blue_rectangle = blueRectangle.getBoundingClientRect();
 
   console.log(
-    "I am the blue rectangle and I am this fat: ",
+    'I am the blue rectangle and I am this fat: ',
     width_of_blue_rectangle.right
   );
 
   if (position.right > width_of_blue_rectangle.right) {
-    console.log("I am too far!!!!!");
+    console.log('I am too far!!!!!');
     TweenMax.to(unicorn, time, { x: 0 });
   }
 };
