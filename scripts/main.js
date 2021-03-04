e1bf068cb322bb726f1a1a5e35ec92b782c8bc32
patrickgrey <websemantic@yahoo.co.uk>
2021-03-04 18:01:04 +0000

    Added upload page

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,33 @@
     const innerWidth = window.innerWidth;
     link.href = link.href.replace("w_1024", `w_${innerWidth}`);
   }
+
+  const superSecretWord = "rpgr";
+  let secretInput = "";
+  const ruth = document.querySelector("h1 span:nth-child(1)");
+  const grey = document.querySelector("h1 span:nth-child(2)");
+  const portfolio = document.querySelector("h1 span:nth-child(3)");
+
+  function handleSpanClick(_letter) {
+    secretInput += _letter;
+    console.log(secretInput);
+    if (secretInput === superSecretWord) {
+      console.log("YOU MAY ENTERRRR!!");
+      window.location.href = "upload/";
+    }
+  }
+
+  ruth.addEventListener("click", function (event) {
+    handleSpanClick("r");
+  });
+
+  portfolio.addEventListener("click", function (event) {
+    handleSpanClick("p");
+  });
+
+  grey.addEventListener("click", function (event) {
+    handleSpanClick("g");
+  });
 })(window, document);
 
 const tobii = new Tobii();
---
+++ upload/index.md
@@ -0,0 +1,6 @@
+---
+layout: layouts/upload.njk
+title: Upload
+---
+
+Upload your groovy art here!
